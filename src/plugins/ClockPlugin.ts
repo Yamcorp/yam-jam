4e1e2e564028ec0537e972c86eb8615c3a05d79e
Colin Williams <colin.williams.dev@gmail.com>
2025-04-22 01:25:23 -0500

    :sparkles: day night cycle working

--- src/plugins/ClockPlugin.ts
+++ src/plugins/ClockPlugin.ts
@@ -1,8 +1,18 @@
 import ClockSingleton from "../scenes/Clock";
 
+const CLOCK_CONSTANTS = {
+    CYCLE_LENGTH: 24000,
+    DAY_LENGTH: 12000,
+    NIGHT_LENGTH: 12000,
+    SUNSET_WARNING: 9000
+};
+
 export default class ClockPlugin extends Phaser.Plugins.BasePlugin {
-    private clockSceneSingleton!: Phaser.Scene;
     public gameClock!: Phaser.Time.Clock;
+    private _clockSceneSingleton!: Phaser.Scene;
+    private _isDay: boolean = true;
+    private _isNight: boolean = false;
+    private _warningSoundPlayed: boolean = false;
 
     constructor(pluginManager: Phaser.Plugins.PluginManager) {
         super(pluginManager);
@@ -15,20 +25,21 @@ export default class ClockPlugin extends Phaser.Plugins.BasePlugin {
     override init(): void {
         console.log("ClockPlugin initialized");
         this.game.scene.add("ClockSingleton", new ClockSingleton());
-        this.clockSceneSingleton = this.pluginManager.game.scene.getScene("ClockSingleton");
+        this._clockSceneSingleton = this.pluginManager.game.scene.getScene("ClockSingleton");
     }
 
     override start(): void {
-        if (!this.clockSceneSingleton) {
+        if (!this._clockSceneSingleton) {
             console.error("ClockSingleton scene not found!");
             return;
         }
 
-        if (this.clockSceneSingleton.sys.isActive()) {
-            this.startLogger();
+        if (this._clockSceneSingleton.sys.isActive()) {
+            this.gameClock = this._clockSceneSingleton.time;
+            this.startDayNightCycle();
         } else {
             console.warn("Clock Singleton not active! \n --waiting for it to emit ready--");
-            this.clockSceneSingleton.events.once("ready", () => this.startLogger());
+            this._clockSceneSingleton.events.once("ready", () => this.startDayNightCycle());
         }
 
 
@@ -47,12 +58,87 @@ export default class ClockPlugin extends Phaser.Plugins.BasePlugin {
     //#region Public API -----------------------------------
     // -----------------------------------------------------
 
-    getTime(): number {
+    // --~~~~~~~~~~~~~~~
+    // -- Events --------
+    // --~~~~~~~~~~~~~~~
+
+    startDayNightCycle(): void {
+        this.gameClock.addEvent({
+            delay: 3000,
+            loop: true,
+            callback: () => {
+                const timeInCycle = this.getTimeInCycle();
+                // logs
+                console.log(`Time in cycle: ${timeInCycle}`);
+                console.log(`Elapsed Time: ${this.getElapsedTime()}`);
+
+                // Day to Night
+                if (timeInCycle >= CLOCK_CONSTANTS.DAY_LENGTH && this._isDay) {
+                    this._clockSceneSingleton.events.emit("dayCycleEnd");
+                    this._isDay = false;
+                    this._isNight = true;
+                    this._warningSoundPlayed = false;
+                    console.log("🌙Night has fallen!");
+
+                    // TODO: this.startNight();
+                }
+
+                // Night to Day
+                if (timeInCycle < CLOCK_CONSTANTS.DAY_LENGTH && this._isNight) {
+                    this._clockSceneSingleton.events.emit("nightCycleEnd");
+                    this._isDay = true;
+                    this._isNight = false;
+                    this._warningSoundPlayed = false;
+                    console.log("☀️Day has broken!🐤");
+
+                    this._clockSceneSingleton.sound.play("yeet", { volume: 0.5 });
+
+                    // TODO: this.startDay();
+                }
+
+                // Sunset Warning 3/4 Day
+                if (timeInCycle >= CLOCK_CONSTANTS.SUNSET_WARNING
+                    && timeInCycle < CLOCK_CONSTANTS.DAY_LENGTH
+                    && this._isDay && !this._isNight) {
+                    this._clockSceneSingleton.events.emit("sunsetWarning");
+                    console.log("🌅Sunset warning!");
+
+                    // TODO: play warning sound
+                    this._clockSceneSingleton.sound.play("fart-00", { volume: 0.5 });
+                    this._warningSoundPlayed = true;
+                }
+            },
+        });
+    }
+
+    /**
+     * this method will simultaneously start the day and end the night cycles
+     */
+    startDay() {
+        // TODO: begin day cycle
+    }
+
+    /**
+     * this method will simultaneously start the night and end the day cycles
+     */
+    startNight() {
+        // TODO: begin night cycle
+    }
+
+    // --~~~~~~~~~~~~~~~
+    // -- Utility -------
+    // --~~~~~~~~~~~~~~~
+
+    public getTime(): number {
         return this.gameClock.now;
     }
 
     getElapsedTime(): number {
-        return this.gameClock.now - this.gameClock.startTime;
+        return Math.floor(this.gameClock.now - this.gameClock.startTime);
+    }
+
+    getTimeInCycle(): number {
+        return this.getElapsedTime() % CLOCK_CONSTANTS.CYCLE_LENGTH;
     }
 
     pauseTime(): void {
@@ -73,38 +159,6 @@ export default class ClockPlugin extends Phaser.Plugins.BasePlugin {
 
     resumeAllEvents() {
         this.gameClock.paused = false;
-    }
-
-    /**
-     * this is a test method to log the clock ticks to the console
-     */
-    startLogger() {
-        this.gameClock = this.clockSceneSingleton.time;
-        console.log(`logger started at -> ${this.gameClock.now}`);
-        console.dir(this.gameClock);
-
-        this.gameClock.addEvent({
-            delay: 2000,
-            loop: true,
-            callback: () => {
-                console.log(`Global Clock Tick : @ ${this.gameClock.now} \n Elapsed: ${this.getElapsedTime().toFixed(1)}`);
-                console.dir(this.gameClock);
-            },
-        });
-    }
-
-    /**
-     * this method will simultaneously start the day and end the night cycles
-     */
-    startDay() {
-        // TODO: begin day cycle
-    }
-
-    /**
-     * this method will simultaneously start the night and end the day cycles
-     */
-    startNight() {
-        // TODO: begin night cycle
     }
 
     // -----------------------------------------------------
