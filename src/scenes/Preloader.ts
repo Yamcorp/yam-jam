507c8b832cc893ef05b45faecd94b4772441a62c
Colin Williams <colin.williams.dev@gmail.com>
2025-04-22 01:25:04 -0500

    :busts_in_silhouette: sneaky

--- src/scenes/Preloader.ts
+++ src/scenes/Preloader.ts
@@ -59,6 +59,9 @@ export class Preloader extends Scene
             frameWidth: 16,
             frameHeight: 20,
         });
+
+        this.load.audio("fart-00", "/audio/fart-00.mp3");
+        this.load.audio("yeet", "/audio/yeet.mp3");
     }
 
     create ()
