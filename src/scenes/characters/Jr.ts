d9402ebd36979f136815e3b27341b6ec35d159d3
kalypsohoman <catcatmcgee@gmail.com>
2025-04-25 12:31:42 -0500

    Add healthy dialogue

--- src/scenes/characters/Jr.ts
+++ src/scenes/characters/Jr.ts
@@ -21,6 +21,36 @@ export class Jr extends Phaser.Physics.Arcade.Sprite {
       return;
   }
 
+  public getHealthyDialogue() {
+    const dialogueList = [
+      "I love you dad.",
+      "I think farming is kinda cool.",
+      "I want to make some irl friends... How do I do that?",
+      "I made you some yam pie. I hope you like it.",
+      "I chased off some crows today to try to help with our yam debt.",
+      "I miss mom.",
+      "I'm tired of eating yams all the time. Remember that one time we had corn??",
+      "This rock isn't so bad when we're hanging out together...",
+      "I found some ice cream! No, don't eat it! It's fake ice cream from the well.",
+      "My imaginary friend Bento was pretty angry with me today.",
+      "My imaginary friend Bento told me the funniest joke earlier. I can't remember it but maybe he can tell you sometime.",
+      "Dad, what's a baseball?",
+      "Dad, do you remember that doll I used to have? What ever happened to it?",
+      "Can we pretend to go the beach sometime?",
+      "Television sounds so weird compared to TubeStream",
+      "What's a cake? Oh! Maybe if you're not so busy one of these days, we can make one!",
+      "Dad, will you tuck me in?",
+      "Dad, will you tell me a story?",
+      "What was the rock that you grew up on like?",
+      "So you didn't have to eat yams when you were younger...?",
+      "...from a book? No way, that's crazy!",
+      "Let's play a game! I know you're tired, but pleassse???.",
+    ]
+
+    const randomIndex = Math.floor(Math.random() * dialogueList.length)
+    return dialogueList[randomIndex]
+  }
+
   public getCyborgDialogue() {
     let dialogue;
     switch (this.state) {
