4948eedea8bccdea55aa4d11ec8615bb64d9cf36
Colin Williams <colin.williams.dev@gmail.com>
2025-04-24 22:06:09 -0500

    :loud_sound: running sound load preload

--- src/scenes/Preloader.ts
+++ src/scenes/Preloader.ts
@@ -62,6 +62,7 @@ export class Preloader extends Scene
 
         this.load.audio("fart-00", "/audio/fart-00.mp3");
         this.load.audio("yeet", "/audio/yeet.mp3");
+        this.load.audio("running-00", "/audio/running-00.mp3");
 
         this.load.image('tiles', 'atlas.png');
         this.load.tilemapTiledJSON('tilemap', 'level.json');
