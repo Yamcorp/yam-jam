6bd87506cd2a1cacccb5b80b256440fb3ac63176
Colin Williams <colin.williams.dev@gmail.com>
2025-04-26 03:30:59 -0500

    :sparkles: title screen controls minimum

--- src/scenes/MainMenu.ts
+++ src/scenes/MainMenu.ts
@@ -2,12 +2,14 @@ import { Scene, GameObjects } from 'phaser';
 
 const TITLE_TEXT = `YAMBORI`;
 const SUBTITLE_TEXT = 'Click to Start';
+const CONTROLS_TEXT = 'Controls: Arrow keys to move, Mouse to aim and throw, E to plant and harvest yams';
 
 export class MainMenu extends Scene
 {
     background: GameObjects.Image;
     logo: GameObjects.Text;
     title: GameObjects.Text;
+    controls: GameObjects.Text;
 
     constructor ()
     {
@@ -22,12 +24,18 @@ export class MainMenu extends Scene
             fontFamily: 'Arial Black', fontSize: 78, color: '#ffffff',
             align: 'center'
         }).setOrigin(0.5);
-        this.logo.setShadow(2, 2, '#000000', 0, true, true );
+        this.logo.setShadow(4, 2, '#000000', 0, true, true );
         // this.logo.setInteractive();
 
         this.title = this.add.text(512, 460, SUBTITLE_TEXT, {
             fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
             stroke: '#000000', strokeThickness: 8,
+            align: 'center'
+        }).setOrigin(0.5);
+
+        this.controls = this.add.text(512, 630, CONTROLS_TEXT, {
+            fontFamily: 'Arial Black', fontSize: 12, color: '#ffffff',
+            stroke: '#000000', strokeThickness: 3,
             align: 'center'
         }).setOrigin(0.5);
 
