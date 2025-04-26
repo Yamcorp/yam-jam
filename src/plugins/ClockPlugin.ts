8ac7841c95e146cb232ac4b4964d8a26385605da
Colin Williams <colin.williams.dev@gmail.com>
2025-04-26 02:32:43 -0500

    :boom: passable clock day/night, its bad tho lol

--- src/plugins/ClockPlugin.ts
+++ src/plugins/ClockPlugin.ts
@@ -3,18 +3,19 @@ import DataStorePlugin from "./DataStorePlugin";
 
 export const CLOCK_CONSTANTS = {
     CYCLE_LENGTH: 48000,
-    DAY_LENGTH: 2400,
+    DAY_LENGTH: 24000,
     NIGHT_LENGTH: 24000,
-    SUNSET_WARNING: 4500
+    SUNSET_WARNING: 18000
 };
 
 export default class ClockPlugin extends Phaser.Plugins.BasePlugin {
     public gameClock!: Phaser.Time.Clock;
     private _clockSceneSingleton!: Phaser.Scene;
+    private _dataStorePlugin!: DataStorePlugin | null;
     private _isDay: boolean = true;
     private _isNight: boolean = false;
-    private _currentTime!: number;
-    private _pausedTime!: number;
+    private _dayStartTime!: number;
+    private _dayTime!: number;
 
     /**
      * Audio
@@ -34,6 +35,7 @@ export default class ClockPlugin extends Phaser.Plugins.BasePlugin {
     override init(): void {
         this.game.scene.add("ClockSingleton", new ClockSingleton());
         this._clockSceneSingleton = this.pluginManager.game.scene.getScene("ClockSingleton");
+        this._dataStorePlugin = this.pluginManager.get('DataStorePlugin') as DataStorePlugin || null;
     }
 
     override start(): void {
@@ -47,6 +49,8 @@ export default class ClockPlugin extends Phaser.Plugins.BasePlugin {
 
         if (this._clockSceneSingleton.sys.isActive()) {
             this.gameClock = this._clockSceneSingleton.time;
+            console.log(`~DAY START VALUE IS INITIAL SET~ ${this.gameClock.now}`);
+            this._dayStartTime = this.gameClock.now;
             this.startDayNightCycle();
         } else {
             console.warn("Clock Singleton not active! \n --waiting for it to emit ready--");
@@ -76,11 +80,12 @@ export default class ClockPlugin extends Phaser.Plugins.BasePlugin {
             delay: 3000,
             loop: true,
             callback: () => {
+                console.log(`!! now in event loop !! - ${this.gameClock.now}`);
+                // this._dayStartTime = this.gameClock.now;
                 const timeInCycle = this.getTimeInCycle();
                 // logs
                 console.log(`Time in cycle: ${timeInCycle}`);
                 console.log(`Elapsed Time: ${this.getElapsedTime()}`);
-                console.log(`this.gameClock.now: ${this.gameClock.now}`);
 
                 // Day to Night
                 if (timeInCycle >= CLOCK_CONSTANTS.DAY_LENGTH && this._isDay) {
@@ -109,10 +114,13 @@ export default class ClockPlugin extends Phaser.Plugins.BasePlugin {
                 if (timeInCycle >= CLOCK_CONSTANTS.SUNSET_WARNING
                     && timeInCycle < CLOCK_CONSTANTS.DAY_LENGTH
                     && this._isDay && !this._isNight) {
-                    this._clockSceneSingleton.events.emit("sunsetWarning");
-                    console.log("🌅Sunset warning!");
-
-                    this._warningSoundPlayed = true;
+                        if(!this._warningSoundPlayed) {
+                            this._clockSceneSingleton.events.emit("sunsetWarning");
+                            console.log("🌅Sunset warning!");
+                            this._warningSoundPlayed = true;
+                            // TODO: this scene should be renamed to SunsetScene
+                            this._clockSceneSingleton.scene.launch("NightScene");
+                        }
                 }
             },
         });
@@ -122,12 +130,19 @@ export default class ClockPlugin extends Phaser.Plugins.BasePlugin {
      * this method will simultaneously start the day and end the night cycles
      */
     startDay() {
-      this._morningSound.play();
-      this._clockSceneSingleton.scene.stop("NightScene");
-      const dataStore = this.pluginManager.get('DataStorePlugin') as DataStorePlugin || null;
-      if (dataStore) {
-        dataStore.dayPassed();
-      }
+        console.log(`DAY IS GETTING RESET`);
+        this._dayStartTime = this.gameClock.now;
+        // this._morningSound.play();
+        this._clockSceneSingleton.scene.stop("NightScene");
+    }
+
+    /**
+     * this method will restart the day and the event loop
+     */
+    restartDay() {
+        console.log(`🌻🌻DAY IS GETTING RESTARTED`);
+        this.startDay();
+        this.resumeAllEvents();
     }
 
     /**
@@ -135,7 +150,12 @@ export default class ClockPlugin extends Phaser.Plugins.BasePlugin {
      */
     startNight() {
         this._nightSound.play();
-        this._clockSceneSingleton.scene.launch("NightScene");
+        // this._clockSceneSingleton.scene.launch("NightScene");
+        // TODO: Data Store Access and dayPassed call should be moved to NightScene?
+        // const dataStore = this.pluginManager.get('DataStorePlugin') as DataStorePlugin || null;
+        if (this._dataStorePlugin) {
+            this._dataStorePlugin.dayPassed(this.pauseAllEvents.bind(this));
+        }
     }
 
     // --~~~~~~~~~~~~~~~
@@ -143,7 +163,7 @@ export default class ClockPlugin extends Phaser.Plugins.BasePlugin {
     // --~~~~~~~~~~~~~~~
 
     getElapsedTime(): number {
-        return Math.floor(this.gameClock.now - this.gameClock.startTime);
+        return Math.floor(this.gameClock.now - this._dayStartTime);
     }
 
     getTimeInCycle(): number {
@@ -152,13 +172,12 @@ export default class ClockPlugin extends Phaser.Plugins.BasePlugin {
 
     pauseAllEvents(): void {
         console.log("SDFSADFASDFASDFSD:");
-        console.log(this._currentTime);
-        this._currentTime = this.gameClock.now;
+        console.log(this._dayTime);
+        this._dayTime = this.gameClock.now;
         this.gameClock.paused = true;
     }
 
     resumeAllEvents(): void {
-        this.gameClock.now = this._currentTime;
         this.gameClock.paused = false;
     }
 
